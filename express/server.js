92428fe721c44492a2db7ef0ca67359896ecd2f2
mhe <mail@marcus-herrmann.com>
2019-01-24 17:07:46 +0100

    Tests simple redirect

--- express/server.js
+++ express/server.js
@@ -7,10 +7,10 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
+  res.redirect('https://marcus.io/a11yone');
   res.end();
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
